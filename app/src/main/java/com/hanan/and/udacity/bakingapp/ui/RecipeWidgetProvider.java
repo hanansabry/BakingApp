828394853749f5c726e9923a66348141317d74f2
Hanan Sabry <eng.hanan.sabry@gmail.com>
2018-04-03 01:47:54 +0200

    Adding initial app widget

--- app/src/main/java/com/hanan/and/udacity/bakingapp/ui/RecipeWidgetProvider.java
+++ app/src/main/java/com/hanan/and/udacity/bakingapp/ui/RecipeWidgetProvider.java
@@ -1,8 +1,11 @@
 package com.hanan.and.udacity.bakingapp.ui;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import com.hanan.and.udacity.bakingapp.R;
@@ -19,6 +22,12 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        //construct the click handler for the widget
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -40,6 +49,12 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        updateAppWidget(context, appWidgetManager, appWidgetId);
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
     }
 }
 
